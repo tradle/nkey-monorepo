e1a65135d2a8edf975e2c243c14784e1f93fff2d
Mark Vayngrib <mark@tradle.io>
2016-08-08 12:31:29 -0400

    wrap instead of monkeypatching

--- packages/nkey/index.js
+++ packages/nkey/index.js
@@ -4,59 +4,73 @@ const clone = require('xtend')
 const BASE_PROPS = ['pub', 'priv', 'type']
 
 exports.wrap = wrap
+exports.wrapAPI = wrapAPI
+exports.wrapInstance = wrap
 exports.asyncify = asyncify
 
-function wrap (api) {
-  if (api.genSync) {
-    const genSync = api.genSync
-    api.genSync = function () {
-      const result = genSync.apply(this, arguments)
-      result.isPrivateKey = true
-      return result
-    }
-  }
+function wrap (obj) {
+  return obj.gen || obj.genSync ? wrapAPI(obj) : wrapInstance(obj)
+}
 
-  ;['gen', 'sign', 'verify'].forEach(method => {
-    if (!api[method] && api[method + 'Sync']) {
-      api[method] = asyncify(api[method + 'Sync'])
+function wrapInstance (instance) {
+  const wrapper = {}
+  Object.keys(instance).forEach(k => {
+    if (k === 'signSync' || k === 'verifySync') {
+      setReadonly(wrapper, k, instance[k].bind(instance))
+      addAsync(wrapper, k === 'signSync' ? 'sign' : 'verify')
+    } else if (k === 'toJSON') {
+      setReadonly(wrapper, 'toJSON', function () {
+        return clone(customProps, instance.toJSON.apply(instance, arguments))
+      })
+    } else {
+      setReadonly(wrapper, k, typeof instance[k] === 'function' ? instance[k].bind(instance) : instance[k])
     }
   })
 
-  if (api.sign && !api.set && !api.get) {
-    const customProps = {}
-    api.set = function (k, v) {
-      customProps[k] = v
-      return this
+  const customProps = {}
+  setReadonly(wrapper, 'set', function (k, v) {
+    customProps[k] = v
+    return this
+  })
+
+  setReadonly(wrapper, 'get', function (k) {
+    return customProps[k]
+  })
+
+  return wrapper
+}
+
+function wrapAPI (api) {
+  const wrapper = {}
+  Object.keys(api).forEach(k => {
+    if (k === 'genSync') {
+      setReadonly(wrapper, 'genSync', function () {
+        const result = api[k].apply(api, arguments)
+        result.isPrivateKey = true
+        return result
+      })
+
+      addAsync(wrapper, 'gen')
+    } else if (k === 'fromJSON') {
+      setReadonly(wrapper, 'fromJSON', function (json) {
+        const result = api.fromJSON.apply(api, arguments)
+        if (result.set) {
+          Object.keys(json).forEach(k => {
+            if (!result[k] && BASE_PROPS.indexOf(k) === -1) {
+              result.set(k, json[k])
+            }
+          })
+        }
+
+        result.isPrivateKey = !!json.priv
+        return result
+      })
+    } else {
+      setReadonly(wrapper, k, typeof api[k] === 'function' ? api[k].bind(api) : api[k])
     }
+  })
 
-    api.get = function (k) {
-      return customProps[k]
-    }
-
-    const toJSON = api.toJSON
-    api.toJSON = function () {
-      return clone(customProps, toJSON.apply(this, arguments))
-    }
-  }
-
-  if (api.fromJSON) {
-    const fromJSON = api.fromJSON
-    api.fromJSON = function (json) {
-      const result = fromJSON.apply(this, arguments)
-      if (result.set) {
-        Object.keys(json).forEach(k => {
-          if (!result[k] && BASE_PROPS.indexOf(k) === -1) {
-            result.set(k, json[k])
-          }
-        })
-      }
-
-      result.isPrivateKey = !!json.priv
-      return result
-    }
-  }
-
-  return api
+  return wrapper
 }
 
 function asyncify (fn) {
@@ -72,4 +86,20 @@ function asyncify (fn) {
 
     process.nextTick(() => cb(null, result))
   }
+}
+
+function addAsync (obj /* methods */) {
+  [].slice.call(arguments, 1).forEach(method => {
+    if (!obj[method] && obj[method + 'Sync']) {
+      setReadonly(obj, method, asyncify(obj[method + 'Sync']))
+    }
+  })
+}
+
+function setReadonly (obj, k, v) {
+  Object.defineProperty(obj, k, {
+    enumerable: true,
+    writable: false,
+    value: v
+  })
 }
